747899a736bb1acf001678a71040c115cb5796a9
melkiyhellkin <smallmelkiyhellkin@users.noreply.github.com>
2020-06-03 16:02:29 +0300

    lolik

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-basedir = os.path.abspath(os.path.dirname(file))
+basedir = os.path.abspath(os.path.dirname(__file__))
 project_dir=os.path.join(basedir, 'app')
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
